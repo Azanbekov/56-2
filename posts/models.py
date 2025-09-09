d08532948ac47be1c07c1140cb2f0899c2d03486
Azanbekov <erzanazanbekov@gmail.com>
2025-09-09 15:52:10 +0600

    hw3 added

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models. Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(felf):
+        return self.name
+
+class Tag(models. Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
+    image = models.ImageField(null=True,blank=True)
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=550, null=True, blank = True)
     rate = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now = True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
    
     def  __str__(self):
--- posts/views.py
+++ posts/views.py
@@ -11,3 +11,7 @@ def html_view(request):
 def post_list_view(request):
     posts = Post.objects.all()
     return render (request, "posts/post_list.html", context={"posts_list": posts})
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "posts/post_detail.html", context={"post": post})
